ad1e8e6a4365a7010d2280979d98fa6561ca2f12
Manuel Kyd Thomas Nagpala <kydnagpala.dev@gmail.com>
2025-12-18 04:19:55 +0800

    feat: add relation for permission, user and moduleorder

---
+++ app/Models/Role.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    /**
+     * Get the permissions for the role.
+     */
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class, 'role_permission');
+    }
+
+    /**
+     * Get the users for the role.
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'user_role');
+    }
+
+    /**
+     * Get the module order for the role.
+     */
+    public function moduleOrder(): HasMany
+    {
+        return $this->hasMany(RoleModuleOrder::class)->orderBy('order');
+    }
+}
