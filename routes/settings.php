b609290817da6c6c4bf8893441591a24814cbe63
Manuel Kyd Thomas Nagpala <kydnagpala.dev@gmail.com>
2025-12-18 04:03:10 +0800

    feat: add Role, permission route

--- routes/settings.php
+++ routes/settings.php
@@ -30,11 +30,81 @@ Route::middleware('auth')->group(function () {
         ->name('two-factor.show');
 
     // User Management Routes
-    Route::resource('settings/users', UserController::class);
+    // IMPORTANT: Index route must come first
+    Route::middleware('permission:users.view')->group(function () {
+        Route::get('settings/users', [UserController::class, 'index'])->name('users.index');
+    });
+
+    // IMPORTANT: Create route MUST come before {user} route to prevent route collision
+    // Laravel matches routes top-to-bottom, so /settings/users/create must be
+    // registered before /settings/users/{user} to avoid "create" being treated as a user ID
+    Route::middleware('permission:users.create')->group(function () {
+        Route::get('settings/users/create', [UserController::class, 'create'])->name('users.create');
+        Route::post('settings/users', [UserController::class, 'store'])->name('users.store');
+    });
+
+    // Show route comes after create to prevent matching "create" as a user ID
+    Route::middleware('permission:users.view')->group(function () {
+        Route::get('settings/users/{user}', [UserController::class, 'show'])->name('users.show');
+    });
+
+    Route::middleware('permission:users.edit')->group(function () {
+        Route::get('settings/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+        Route::put('settings/users/{user}', [UserController::class, 'update'])->name('users.update');
+        Route::patch('settings/users/{user}', [UserController::class, 'update']);
+    });
+
+    Route::delete('settings/users/{user}', [UserController::class, 'destroy'])
+        ->middleware('permission:users.delete')
+        ->name('users.destroy');
 
     // Role Management Routes
-    Route::resource('settings/roles', RoleController::class);
+    Route::middleware('permission:roles.view')->group(function () {
+        Route::get('settings/roles', [RoleController::class, 'index'])->name('roles.index');
+    });
+
+    // Create route MUST come before {role} route
+    Route::middleware('permission:roles.create')->group(function () {
+        Route::get('settings/roles/create', [RoleController::class, 'create'])->name('roles.create');
+        Route::post('settings/roles', [RoleController::class, 'store'])->name('roles.store');
+    });
+
+    Route::middleware('permission:roles.view')->group(function () {
+        Route::get('settings/roles/{role}', [RoleController::class, 'show'])->name('roles.show');
+    });
+
+    Route::middleware('permission:roles.edit')->group(function () {
+        Route::get('settings/roles/{role}/edit', [RoleController::class, 'edit'])->name('roles.edit');
+        Route::put('settings/roles/{role}', [RoleController::class, 'update'])->name('roles.update');
+        Route::patch('settings/roles/{role}', [RoleController::class, 'update']);
+    });
+
+    Route::delete('settings/roles/{role}', [RoleController::class, 'destroy'])
+        ->middleware('permission:roles.delete')
+        ->name('roles.destroy');
 
     // Permission Management Routes
-    Route::resource('settings/permissions', PermissionController::class);
+    Route::middleware('permission:permissions.view')->group(function () {
+        Route::get('settings/permissions', [PermissionController::class, 'index'])->name('permissions.index');
+    });
+
+    // Create route MUST come before {permission} route
+    Route::middleware('permission:permissions.create')->group(function () {
+        Route::get('settings/permissions/create', [PermissionController::class, 'create'])->name('permissions.create');
+        Route::post('settings/permissions', [PermissionController::class, 'store'])->name('permissions.store');
+    });
+
+    Route::middleware('permission:permissions.view')->group(function () {
+        Route::get('settings/permissions/{permission}', [PermissionController::class, 'show'])->name('permissions.show');
+    });
+
+    Route::middleware('permission:permissions.edit')->group(function () {
+        Route::get('settings/permissions/{permission}/edit', [PermissionController::class, 'edit'])->name('permissions.edit');
+        Route::put('settings/permissions/{permission}', [PermissionController::class, 'update'])->name('permissions.update');
+        Route::patch('settings/permissions/{permission}', [PermissionController::class, 'update']);
+    });
+
+    Route::delete('settings/permissions/{permission}', [PermissionController::class, 'destroy'])
+        ->middleware('permission:permissions.delete')
+        ->name('permissions.destroy');
 });
