e11371bde8c57f530809ebb02817dd7697a6bedf
Manuel Kyd Thomas Nagpala <kydnagpala.dev@gmail.com>
2025-12-18 04:19:12 +0800

    feat: add create relation for roles

---
+++ app/Models/Permission.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Permission extends Model
+{
+    protected $fillable = [
+        'name',
+        'module',
+        'action',
+        'display_name',
+        'description',
+    ];
+
+    /**
+     * Get the roles for the permission.
+     */
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class, 'role_permission');
+    }
+}
